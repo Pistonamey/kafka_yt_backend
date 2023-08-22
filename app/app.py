01968d0fdb7986654a1576422ba4895a15be9b9c
Pistonamey <ameyshinde11111@gmail.com>
2023-08-22 17:31:14 -0500

    handle while

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from flask import Flask,request,jsonify,json
 from flask_cors import CORS
 import os
 import threading
+import time
 from confluent_kafka import Producer, Consumer
 from app.methods import get_confluent_config
 from app.methods import kafka_consumer
@@ -20,34 +21,43 @@ with app.app_context():
     thread = threading.Thread(target=kafka_consumer_thread)
     thread.start()
 
-@app.route('/get_sentiment',methods=['POST'])
+@app.route('/get_sentiment', methods=['POST'])
 def sentiment_analysis():
-
-    if request.method=='POST':
+    if request.method == 'POST':
         video_id = request.form['video_id']
         print(video_id)
-        # result = get_sentiment.main(video_id)
-        # return jsonify({'result': result})
         producer.produce('youtube-urls', key=video_id, value=video_id)
         producer.flush()
         consumer = Consumer(get_confluent_config.get_config()[1])
         consumer.subscribe(['sentiment-results'])
 
-        # Poll the result topic for the specific video ID
-        # (In real-world scenarios, you'd probably want to set a timeout or handle this more gracefully)
-        while True:
-            msg = consumer.poll(1.0)
-            if msg is None:
-                continue
-            if msg.error():
-                print("Consumer error: {}".format(msg.error()))
-                continue
-            if msg.key().decode('utf-8') == video_id:
-                sentiment_result = msg.value().decode('utf-8')
-                return jsonify({'result': sentiment_result})
+        # Poll the result topic for the specific video ID with a timeout
+        timeout = 10  # Example: 10 seconds, adjust as needed
+        start_time = time.time()
 
+        try:
+            while True:
+                elapsed_time = time.time() - start_time
+                if elapsed_time >= timeout:
+                    # If timeout is reached, break the loop
+                    break
+
+                msg = consumer.poll(1.0)
+                if msg is None:
+                    continue
+                if msg.error():
+                    print("Consumer error: {}".format(msg.error()))
+                    continue
+                if msg.key().decode('utf-8') == video_id:
+                    sentiment_result = msg.value().decode('utf-8')
+                    return jsonify({'result': sentiment_result})
+
+        finally:
+            # Always close the consumer, regardless of how the loop exits
+            consumer.close()
 
     return jsonify({'error': 'Method not supported'})
+
         
 
 
